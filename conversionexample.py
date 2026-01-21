e8f7e7fc286c91b2b36ba6ca71fd6ce31415c63f
GMS2805 <mukundshastry9@gmail.com>
2026-01-21 08:39:54 +0530

    Example modules made for notebook 44.

---
+++ conversionexample.py
@@ -0,0 +1,15 @@
+def cel2fah(c):
+    """Converts from Celsius to Fahrenheit."""
+    return 9/5 * c + 32
+
+def fah2cel(f):
+    """Converts from Fahrenheit to Celsius."""
+    return 5/9 * (f - 32)
+
+def km2mi(km):
+    """Converts from kilometers to miles."""
+    return km / 1.60934
+
+def mi2km(mi):
+    """Converts from miles to kilometers."""
+    return mi * 1.60934
